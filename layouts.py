def17d96d5532d85f452bd02fefd6e6282700017
eltarr-kawther <kawther.eltarr@laplateforme.io>
2021-02-17 17:20:41 +0100

    added background image for page1

--- layouts.py
+++ layouts.py
@@ -7,13 +7,23 @@ Created on Wed Feb 17 15:07:33 2021
 import dash_core_components as dcc
 import dash_html_components as html
 
-layout1 = html.Div([
-    html.H3('Welcome'),
+layout1 = html.Div(children=[
+    html.H1('Welcome'),
     dcc.Link('Go to App 2', href='/apps/app2')
-])
+    ], style={
+  'verticalAlign':'middle',
+  'textAlign': 'center',
+  'background-image': 'url("/assets/noah.jpg")',
+  'position':'fixed',
+  'width':'100%',
+  'height':'100%',
+  'top':'0px',
+  'left':'0px',
+  'z-index':'1000'
+})
 
 layout2 = html.Div([
-    html.H3('App 2'),
+    html.H1('App 2'),
     dcc.Dropdown(
         id='app-2-dropdown',
         options=[

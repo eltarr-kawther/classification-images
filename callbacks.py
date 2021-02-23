b0cc75b9edb1df32dec5ad4a32c2c94cd33f17ff
eltarr-kawther <kawther.eltarr@laplateforme.io>
2021-02-23 16:09:01 +0100

    new notebook for data and a few edits

--- callbacks.py
+++ callbacks.py
@@ -4,7 +4,7 @@ Created on Wed Feb 17 15:05:50 2021
 
 @author: straw
 """
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 from app import app
 
 @app.callback(
@@ -12,3 +12,15 @@ from app import app
     Input('app-2-dropdown', 'value'))
 def display_value(value):
     return 'You have selected "{}"'.format(value)
+
+@app.callback(Output('output-data-upload', 'children'),
+              Input('upload-data', 'contents'),
+              State('upload-data', 'filename'),
+              State('upload-data', 'last_modified'))
+def update_output(list_of_contents, list_of_names, list_of_dates):
+    if list_of_contents is not None:
+        children = [
+            parse_contents(c, n, d) for c, n, d in
+            zip(list_of_contents, list_of_names, list_of_dates)]
+        return children
+
